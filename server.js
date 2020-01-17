3c93c260d33d37f0fbbf956ba904706274c82dce
Sheldon Perkins <csperkins18@gmail.com>
2020-01-17 00:28:34 -0500

    post route is working but is not saving to db. TO DO: Get post to save to db.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require('mongoose')
 var User = require('./models/user.js');
 // var bodyParser = require('body-parser');
 var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
 // var MONGODB_URI =  'mongodb://localhost:27017/mon_auth';
 // mongoose.connect(MONGODB_URI, {},  function(error) {
 //     console.log("$$$$$$$$$$$$$$$$$$$",error)
@@ -12,41 +22,85 @@ mongoose.connect(connStr, { useNewUrlParser: true }, function (err) {
     if (err) throw err;
     console.log('Successfully connected to MongoDB');
 });
-
 // create a user a new user
-var testUser = new User({
-    username: 'jmar777',
-    password: 'Password123'
-});
+// var testUser = new User({
+//     username: 'jmar777',
+//     password: 'Password123'
+// });
+// var user = new User({
+//     username = req.body.username,
+//     password = req.body.password
+// });
+// app.post('/login', (req, res) => {
+// //     const username = req.body.username;
+// //   const password = req.body.password;
 
+// //   console.log(username)
+// //   console.log("+++++++++++++++++++++++++++++++")
+// //   console.log(password)
+//     // fetch user and test password verification
+//     User.findOne({ username: req.body.username }, (req, res) => {
+//         // const username = req.body.username;
+//         // const password = req.body.password;
+      
+//         console.log(username)
+//         console.log("+++++++++++++++++++++++++++++++")
+//         console.log(req.password)
+//         if (err) throw err;
+
+//         // test a matching password
+//         user.comparePassword(password, function (err, isMatch) {
+//             if (err) throw err;
+//             console.log('Password', isMatch); // -> Password123: true
+//         });
+
+//         // test a failing password
+//         user.comparePassword(passworrd, function (err, isMatch) {
+//             if (err) throw err;
+//             console.log('Password:', isMatch); // -> 123Password: false
+//         });
+//     });
+// });
+// Route to post our form submission to mongoDB via mongoose
+app.post("/signup", function(req, res) {
+    // Create a new user using req.body
+    User.create(req.body)
+      .then(function(dbUser) {
+        // If saved successfully, send the the new User document to the client
+        console.log(dbUser);
+        res.json(dbUser);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error to the client
+        console.log("Fail");
+        res.json(err);
+      });
+  });
 // save user to database
 // testUser.post(function(err) {
 //     if (err) throw err;
 
-    // fetch user and test password verification
-    User.findOne({ username: 'jmar777' }, function(err, user) {
-        if (err) throw err;
+// // fetch user and test password verification
+// User.findOne({ username: 'jmar777' }, function(err, user) {
+//     if (err) throw err;
 
-        // test a matching password
-        user.comparePassword('Password123', function(err, isMatch) {
-            if (err) throw err;
-            console.log('Password123:', isMatch); // -> Password123: true
-        });
+//     // test a matching password
+//     user.comparePassword('Password123', function(err, isMatch) {
+//         if (err) throw err;
+//         console.log('Password123:', isMatch); // -> Password123: true
+//     });
 
-        // test a failing password
-        user.comparePassword('123Password', function(err, isMatch) {
-            if (err) throw err;
-            console.log('123Password:', isMatch); // -> 123Password: false
-        });
-    });
+//     // test a failing password
+//     user.comparePassword('123Password', function(err, isMatch) {
+//         if (err) throw err;
+//         console.log('123Password:', isMatch); // -> 123Password: false
+//     });
+// });
 // });
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
