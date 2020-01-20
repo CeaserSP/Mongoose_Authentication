8e5fe933fa352c7e71fe6d841b31cf0a9e7c5b9c
Sheldon Perkins <csperkins18@gmail.com>
2020-01-19 22:19:07 -0500

    Post works(it does not work in postman).

--- server.js
+++ server.js
@@ -27,10 +27,14 @@ mongoose.connect(connStr, { useNewUrlParser: true }, function (err) {
 //     username: 'jmar777',
 //     password: 'Password123'
 // });
-// var user = new User({
+// var newUser = new User({
 //     username = req.body.username,
 //     password = req.body.password
 // });
+
+// newUser.save(function(err) {
+//   if (err) throw err;
+// }
 // app.post('/login', (req, res) => {
 // //     const username = req.body.username;
 // //   const password = req.body.password;
@@ -62,20 +66,18 @@ mongoose.connect(connStr, { useNewUrlParser: true }, function (err) {
 //     });
 // });
 // Route to post our form submission to mongoDB via mongoose
-app.post("/signup", function(req, res) {
-    // Create a new user using req.body
-    User.create(req.body)
-      .then(function(dbUser) {
-        // If saved successfully, send the the new User document to the client
-        console.log(dbUser);
-        res.json(dbUser);
-      })
-      .catch(function(err) {
-        // If an error occurs, send the error to the client
-        console.log("Fail");
-        res.json(err);
-      });
-  });
+app.post("/submit", function(req, res) {
+  // Create a new user using req.body
+  User.create(req.body)
+    .then(function(dbUser) {
+      // If saved successfully, send the the new User document to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error to the client
+      res.json(err);
+    });
+});
 // save user to database
 // testUser.post(function(err) {
 //     if (err) throw err;
